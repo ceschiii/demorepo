909b1f9635573eb46b1599947fcce5ea7955b441
ceschiii <hello@cesca.games>
2021-11-15 15:41:49 -0800

    first Unity files commit

---
+++ Assets/scripts/addForce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addForce : MonoBehaviour
+{
+    public float force = 20f;
+    private Rigidbody my_Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        my_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            my_Rigidbody.AddForce(transform.up * force);
+        }
+    }
+}
